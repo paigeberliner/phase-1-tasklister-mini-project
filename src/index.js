e2c49cbd895e28c50c75cc5a97dfbdea0bbdf158
Paige <paigeberliner@gmail.com>
2023-08-18 14:08:22 -0400

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e) => {
+  e.preventDefault()
+  buildToDo(e.target['new-task-description'].value)
+  })
+})
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', removeToDo)
+  p.textContent = ` ${todo} `
+  btn.textContent = 'delete me'
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function removeToDo(e){
+  e.target.parentNode.remove()
+}
